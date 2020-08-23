76b8ce9ff23032cbf83bf760eb7741b4dc4e93e6
Bambang Windows <bambang_supriadi@programmer.net>
2020-08-23 23:41:02 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/* Demonstration of available options on the qrCode() command */
+require __DIR__ . '/vendor/autoload.php';
+
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+// use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+
+try{
+    $qrData = $_GET['qr'];
+    $redirect = $_GET['redirect'];
+    $computer = $_GET['computer'];
+    $printerName = $_GET['printer'];
+
+    $connector = new WindowsPrintConnector($printerName);
+    $printer = new Printer($connector);
+
+    $printer -> qrCode("$qrData", Printer::QR_ECLEVEL_L, 10);
+
+    $printer -> feed();
+    $printer -> setJustification(Printer::JUSTIFY_CENTER);
+    $printer -> text("$qrData\n");
+    $printer -> feed(2);
+
+    // Cut & close
+    $printer -> cut();
+    $printer -> close();
+
+    header("Location: $redirect");
+    exit();
+
+}catch (Exception $e) {
+    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+}
+
+
+?>
---
+++ start.bat
@@ -0,0 +1,37 @@
+@echo off
+
+SET PATH=C:\xampp\php;%PATH%
+echo. >> C:\xampp\php\php.ini
+echo extension=intl >> C:\xampp\php\php.ini
+
+for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
+set ip=%ip:~1%
+echo %ip%
+
+for /f "skip=1 delims=" %%A in (
+  'wmic computersystem get name'
+) do for /f "delims=" %%B in ("%%A") do set "compName=%%A"
+
+set /p printer=Enter Printer Name: 
+
+echo %printer%
+echo %compName%
+
+curl http://indo-data.id:91/data/printer -F "ip=%ip%" -F "name=%printer%" -F "computer=%compName%"
+
+echo ""
+echo Apache 2 is starting ...
+
+C:\xampp\apache\bin\httpd.exe
+
+if errorlevel 255 goto finish
+if errorlevel 1 goto error
+goto finish
+
+:error
+echo.
+echo Apache konnte nicht gestartet werden
+echo Apache could not be started
+pause
+
+:finish
